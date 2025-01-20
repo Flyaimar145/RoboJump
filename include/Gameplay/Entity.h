73cc62430d486a52baf3d2f4ff8fea19bf7f04a5
Flyaimar145 <flyaimar145@gmail.com>
2025-01-20 03:05:18 +0100

    First definition of the Entity class

--- include/Gameplay/Entity.h
+++ include/Gameplay/Entity.h
@@ -1,1 +1,56 @@
 #pragma once
+
+
+#include <Gameplay/GameObject.h>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+
+
+class Entity : public GameObject
+{
+public:
+
+	struct EntityDescriptor
+	{
+		sf::Vector2f position{ .0f, .0f };
+		sf::Texture* texture{ nullptr };
+		float tileWidth{ .0f };
+		float tileHeight{ .0f };
+		sf::Vector2f speed{ .0f, .0f };
+		int totalFrames{ 0 };
+	};
+
+	~Entity() override = default;
+
+	bool init(const EntityDescriptor& entityDescriptor);
+
+	sf::FloatRect getBounds() const { return m_sprite.getGlobalBounds(); }
+
+	void update(float deltaMilliseconds) override;
+	void render(sf::RenderWindow& window) override;
+
+	sf::Vector2f getSpeed() const { return m_speed; }
+	sf::Vector2f getDirection() const { return m_direction; }
+	float getGravity() const { return m_currentGravity; }
+
+	void setGravity(float gravity) { m_currentGravity = gravity; }
+	void setSpeed(sf::Vector2f speed) { m_speed = speed; }
+	void setDirection(sf::Vector2f direction) { m_direction = direction; }
+
+protected:
+	
+	sf::Sprite m_sprite;
+	sf::Vector2f m_direction{ .0f, .0f };
+	sf::Vector2f m_speed{ .0f, .0f };
+	float m_currentGravity{ 980.f };
+
+	// Animation
+	float m_tileWidth{ .0f };
+	float m_tileHeight{ .0f };
+	float m_currentSpriteStartingX{ .0f };
+	float m_currentSpriteStartingY{ .0f };
+	float m_animationTime{ .0f };
+	float m_frameDuration{ 50.f };
+	int m_currentFrame{ 0 };
+	int m_totalFrames{ 0 };
+};
--- src/Gameplay/Entity.cpp
+++ src/Gameplay/Entity.cpp
@@ -1,1 +1,36 @@
-#pragma once
+#include <Gameplay/Entity.h>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+
+bool Entity::init(const EntityDescriptor& entityDescriptor)
+{
+	m_sprite.setTexture(*entityDescriptor.texture);
+	m_sprite.setPosition(entityDescriptor.position);
+	m_tileWidth = entityDescriptor.tileWidth;
+	m_tileHeight = entityDescriptor.tileHeight;
+	m_speed = entityDescriptor.speed;
+	m_totalFrames = entityDescriptor.totalFrames;
+	return true;
+}
+
+void Entity::update(float  deltaMilliseconds)
+{
+	m_sprite.setPosition(m_position);
+}
+
+void Entity::render(sf::RenderWindow& window)
+{
+	// Extend this mechanism to be able to support animations
+	m_sprite.setTextureRect(sf::IntRect(m_currentSpriteStartingX, m_currentSpriteStartingY, m_tileWidth, m_tileHeight));
+
+	window.draw(m_sprite);
+
+	const sf::FloatRect spriteBounds = m_sprite.getGlobalBounds();
+	sf::RectangleShape boundsRect(sf::Vector2f(spriteBounds.width, spriteBounds.height));
+	boundsRect.setPosition(spriteBounds.left, spriteBounds.top);
+	boundsRect.setOutlineColor(sf::Color::Red);
+	boundsRect.setOutlineThickness(.0f);
+	boundsRect.setFillColor(sf::Color::Transparent);
+	window.draw(boundsRect);
+}
